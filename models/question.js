8747ba08170ccbfd887ef039e1e6ca4c31a5c914
Nikki G <nikki.goodridge@gmail.com>
2021-04-03 11:23:45 -0400

    added questions and answers models and started answer logic

---
+++ models/question.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Question extends Model{};
+
+Question.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    greenAnswerChoice: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    yellowAnswerChoice: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    redAnswerChoice: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    blueAnswerChoice: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    questionAsked: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+},
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'question'
+},
+);
+
+module.exports = Question;
---
+++ public/javascript/match-logic.js
@@ -0,0 +1,19 @@
+//Connect to Question.js through routes
+
+//Connect to user-answers.js through routes
+
+//To hold responses from questions & user-answsers GLOBAL
+var //questons
+var //user-answers
+
+//Show user the question (from handlebars)
+
+//Store Answer
+const storeAnswers = () => {
+    //Taking answer response and add one pt to answer color column
+}
+
+//Answer Results
+const answerResults = () => {
+
+}
