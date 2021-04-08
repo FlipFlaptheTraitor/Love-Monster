d8b2dee71f25e4e14fcbef3244d61b69198dd82d
Nikki G <nikki.goodridge@gmail.com>
2021-04-07 21:18:34 -0400

    added answer points constants for answer points

--- public/javascript/match-logic.js
+++ public/javascript/match-logic.js
@@ -2,14 +2,10 @@ const submitButton = document.getElementById('submit-btn')
 const confirmButton = document.getElementById('confirm-btn')
 
 let userScore = 0;
-
-//Connect to user-answers.js through routes
-const routes = require('../../controllers/api/userAnswers');
-//To hold responses from questions & user-answsers GLOBAL
-let userAnswer = routes;
-
-//Show user the question (from handlebars)
-
+let greenAnswers = 3;
+let redAnswers = 2;
+let yellowAnswers = 1;
+let blueAnswers = 0;
 
 //Event Listeners
 submitButton.addEventListener('click', () =>{
@@ -19,7 +15,7 @@ submitButton.addEventListener('click', () =>{
 
 confirmButton.addEventListener(hideBtn);
 
-//Store Answer
+//Store Answers
 const storeAnswers = (userAnswer) => {
     //Taking answer response and add one pt to answer color column
 
