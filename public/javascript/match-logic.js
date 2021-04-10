48715255a3f290c5d54ddd36d1f49362043f7181
Nikki G <nikki.goodridge@gmail.com>
2021-04-10 16:04:03 -0400

    new push

--- public/javascript/match-logic.js
+++ public/javascript/match-logic.js
@@ -1,34 +1,39 @@
 const submitButton = document.getElementById('submit-btn');
-const confirmButton = document.getElementById('confirm-btn');
-let greenAnswers = document.querySelectorAll('input[id="green"]');
+// let greenAnswers = document.querySelectorAll("input[name='answers1']:checked").value;
+let greenAnswers = document.getElementById('green').value;
 let redAnswers = document.querySelectorAll('input[id="red"]');
 let yellowAnswers = document.querySelectorAll('input[id="yellow"]');
 let blueAnswers = document.querySelectorAll('input[id="blue"]');
 
 let userScore = 0;
 
+$("li").each(function(){
+    var $this = $(this),
+    id =$this.attr('id'),
+    
+    if($(this).prop('checked')){
+        console.log()
+    }
+
+})
 
 //Event Listeners
 submitButton.addEventListener('click', () =>{
-    storeAnswers();
-    answerResults();
+    // storeAnswers();
+    // answerResults();
+    console.log(greenAnswers)
 });
 
-confirmButton.addEventListener(hideBtn);
 
 //Store Answers
 const storeAnswers = () => {
-    if (greenAnswers.checked){
+    if (greenAnswers){
         console.log('This works')
     }
 
 }
 
 //Answer Results
-const answerResults = () => {
-
-}
-
-const hideBtn = () =>{
-    confirmButton.classList.add('hide');
-}
+// const answerResults = () => {
+//     if userScore > 
+// }
