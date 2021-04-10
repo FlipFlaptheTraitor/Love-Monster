28b59b163c29210f1cfd449eaf533a969639dbc6
FlipFlaptheTraitor <flipflapthetraitor@protonmail.com>
2021-04-09 22:05:34 -0400

    chat redesign need to change html add to handlebars and and sql

--- public/javascript/chatapp.js
+++ public/javascript/chatapp.js
@@ -1,45 +1,69 @@
   
-const socket = io('http://localhost:3000')
-const messageContainer = document.getElementById('message-container')
-const messageForm = document.getElementById('send-container')
-const messageInput = document.getElementById('message-input')
+const socket = io('http://localhost:3000');
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('chat-messages');
+const roomName = document.getElementById('room-name');
+const userlist = document.getElementById('users');
+const {username, room}= Qs.parse(location.search, {
+  ignoreQueryPrefix: true
+});
 
-// sets up a name to dispay in chat only used temp to check chat function
-// will be replace with users data later 
-// I always picture Howard re-enacting the scence from Mommy Deariest screaming "No window prompts!"
-const name = prompt('What is your name?')
-appendMessage('You joined')
-socket.emit('new-user', name)
+const socket = io();
 
-//sends message to append to html 
-socket.on('chat-message', data => {
-  appendMessage(`${data.name}: ${data.message}`)
-})
-//recieves data from chat msg event 
-socket.on('user-connected', name => {
-  appendMessage(`${name} connected`)
-})
-//
-socket.on('user-disconnected', name => {
-  appendMessage(`${name} disconnected`)
-})
+socket.emit('joinRoom', {username, room});
+//getroom and user info
+socket.on('roomUsers', ({room, users}) =>{
+outputRoomName(room);
+outputUsers(users);
+});
 
-//stops chat from refreshing
-messageForm.addEventListener('submit', e => {
-  e.preventDefault()
-  //takes data from webpage  
-  const message = messageInput.value
-  appendMessage(`You: ${message}`)
-  //sends date from  client to server
-  socket.emit('send-chat-message', message)
-  //empties out input box on webpage
-  messageInput.value = ''
-})
+socket.on('message', message => {
+  console.log(message);
+  outputMessage(message);
+
+  //scroll down
+chatMessages.scrollTop = chatMessages.scrollHeight;
+
+});
 
 
-//appends messages to html file
-function appendMessage(message) {
-  const messageElement = document.createElement('div')
-  messageElement.innerText = message
-  messageContainer.append(messageElement)
+
+chatForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+
+
+  const msg = e.target.elements.msg.value
+
+socket.emit('chatMessage', msg);
+
+//clear input
+e.target.elements.msg.focus();
+
+});
+
+
+function outputMessage(message) {
+  const div =document.createElement('div');
+  div.classList.add('message');
+  div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+  <p class="text">
+    ${message.text}
+  </p>`;
+  document.querySelector('.chat-messages').appendChild(div);
 }
+
+
+
+function outputRoomName(room) {
+roomName.innerText = room;
+
+}
+function outputUsers(users) {
+  userList.innerHTML = `
+  ${users.map(user => `<li>${user.username}</li>`).join('')}
+  `;
+}
+
+
+
