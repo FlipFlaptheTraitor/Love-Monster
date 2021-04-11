ba88ce566f97e9b5ffc822c3ea4c54dfcef5a3e1
FlipFlaptheTraitor <flipflapthetraitor@protonmail.com>
2021-04-10 22:52:34 -0400

    chat

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const commentRoutes = require('./comment-routes');
-
+const chatRoutes = require('./chat-routes');
 router.use('/users', userRoutes);
 router.use('/comments', commentRoutes);
-
+router.use('/chat', chatRoutes);
 module.exports = router;
--- public/javascript/chatapp.js
+++ public/javascript/chatapp.js
@@ -80,11 +80,4 @@ function outputUsers(users) {
   });
 }
 
-//Prompt the user before leave chat room
-document.getElementById('leave-btn').addEventListener('click', () => {
-  const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
-  if (leaveRoom) {
-    window.location = '../index.html';
-  } else {
-  }
-});
+
