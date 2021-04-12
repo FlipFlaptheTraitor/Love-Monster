14087a0d6578abe6c1afb4fbc766b6e67716d6e7
Nikki G <nikki.goodridge@gmail.com>
2021-04-12 00:24:33 -0400

    Add Heroku

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -72,7 +72,7 @@ router.post('/', (req, res) => {
 router.post('/login',  (req, res) => {
     User.findOne({
         where: {
-        username: req.body.username
+        email: req.body.email
         }
     }).then(dbUserData => {
         if (!dbUserData) {
