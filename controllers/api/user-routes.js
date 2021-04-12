5176c87015397145d06d1e136a372020a7115091
Prestongeyer713 <75324665+Prestongeyer713@users.noreply.github.com>
2021-04-12 19:16:20 -0400

    Update user-routes.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -155,4 +155,21 @@ router.delete('/:id', withAuth, (req, res) => {
       }))
   });
 
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    } for user-routes
+
 module.exports = router;
